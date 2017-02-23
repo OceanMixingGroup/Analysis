2671f4887ba7eaa0b401e91dd993fb70fa460b1b
andypicke <andypicke@gmail.com>
2017-02-23 10:00:10 -0700

    new

---
+++ Andy_Pickering/tiwe_patch_gamma/add_binned_to_patches.m
@@ -0,0 +1,73 @@
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~
+%
+% add_binned_to_patches.m
+%
+% Formerly part of Compute_N2_dTdz_patches_tiwe_eachcast.m
+%
+%----------------
+% 2/23/17 - A.Pickering
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~
+%% Add 1m binned data (interpolated to patch locations)
+
+clear ; close all
+
+% patch parameters
+patch_size_min = 0.25 ; % min patch size
+usetemp   = 1 ;         % 1=use pot. temp, 0= use density
+%datdir='/Users/Andy/Cruises_Research/Analysis/Andy_Pickering/tiwe_patch_gamma/data'
+save_dir_patch='/Users/Andy/Cruises_Research/Analysis/Andy_Pickering/tiwe_patch_gamma/data/patches/'
+
+% load binned chameleon data (structure containing all profiles)
+load('/Users/Andy/Cruises_Research/Analysis/Andy_Pickering/tiwe_patch_gamma/data/tiwe_1mavg_combined.mat')
+
+addpath /Users/Andy/Cruises_Research/ChiPod/Cham_Eq14_Compare/mfiles/Patches/code/
+
+hb=waitbar(0)
+for cnum=1:4000
+    
+    waitbar(cnum/4000,hb)
+    
+    clear patches Npatches
+    
+    try
+        
+        % load the patches for this profile
+        load(fullfile(save_dir_patch,['tiwe_cham_minOT_' num2str(100*patch_size_min) '_usetemp_' num2str(usetemp) '_patches_diffn2dtdzgamma_cnum_' num2str(cnum) '.mat']) )
+        
+        % make empty arrays for the binned data
+        Npatches = length(patches.cnum) ;
+        patches.gam_bin = nan*ones(size(patches.p1)) ;
+        patches.n2_bin  = nan*ones(size(patches.p1)) ;
+        patches.dtdz_bin= nan*ones(size(patches.p1)) ;
+        patches.chi_bin = nan*ones(size(patches.p1)) ;
+        patches.eps_bin = nan*ones(size(patches.p1)) ;
+        patches.drhodz_bin=nan*ones(size(patches.p1)) ;
+        
+        % get index for binned profile
+        clear Icham pbin pmn
+        Icham= find(cham.cnum==cnum) ;
+        pbin = cham.P;
+        pmn  = nanmean([patches.p1 patches.p2],2) ;
+        
+        % interp binned data to the patch locations (mean p of each patch)
+        
+        clear ig
+        ig=find(~isnan(pbin)) ;
+        patches.n2_bin    = interp1(pbin(ig) , cham.N2(ig,Icham) , pmn);
+        patches.dtdz_bin  = interp1(pbin(ig) , cham.DTDZ(ig,Icham) , pmn);
+        patches.chi_bin   = interp1(pbin(ig) , cham.CHI(ig,Icham) , pmn);
+        patches.eps_bin   = interp1(pbin(ig) , cham.EPSILON(ig,Icham) , pmn);
+        %    patches.drhodz_bin(ip)= patches.n2_bin(ip) * (nanmean(cham.SIGMA(:,Icham))+1000) / -9.81 ;
+        
+        %     if log10(patches.eps_bin(ip))>-8.5
+        %         patches.gam_bin=ComputeGamma(patches.n2_bin,patches.dtdz_bin,patches.chi_bin,patches.eps_bin);
+        %     end
+        
+        % re-save profile
+        save(fullfile(save_dir_patch,['tiwe_cham_minOT_' num2str(100*patch_size_min) '_usetemp_' num2str(usetemp) '_patches_diffn2dtdzgamma_cnum_' num2str(cnum) '.mat']), 'patches' )
+        
+    end % try
+end % cnum
+delete(hb)
+
+%%
---
+++ Andy_Pickering/tiwe_patch_gamma/add_patch_chi_eps_to_patches_tiwe_each_profile.m
@@ -0,0 +1,67 @@
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~
+%
+% add_patch_chi_eps_to_patches_tiwe_each_profile.m
+%
+% mod from add_patch_chi_eps_to_patches_tiwe.m
+%
+% Add chi and epsilon values computed for patches (in
+% Run_tiwe_AP_forPatches.m) to our patches structure.
+%
+% Used to be part of Compute_N2_dTdz_patches_tiwe.m
+%
+%-----------
+% 2/23/17 - A.Pickering
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~
+%%
+
+clear ; close all
+
+% patch options
+patch_size_min = 0.25  % min patch size
+usetemp = 1
+save_dir_patch='/Users/Andy/Cruises_Research/Analysis/Andy_Pickering/tiwe_patch_gamma/data/patches/'
+
+% folder for chameleon data processed over patches (Run_tiwe_AP_forPatches.m)
+data_dir=fullfile('/Users/Andy/Cruises_Research/Analysis/Andy_Pickering/tiwe_patch_gamma/data/avg_patch',['minOT_' num2str(100*patch_size_min) '_usetemp_' num2str(usetemp)])
+
+addpath /Users/Andy/Cruises_Research/ChiPod/Cham_Eq14_Compare/mfiles/Patches/code/
+
+hb=waitbar(0,'compiling patch data from all profiles');
+
+for cnum=1:4000
+    
+    waitbar(cnum/4000,hb)
+    
+    try
+        % load the patches for this profile
+        clear patches
+        load(fullfile(save_dir_patch,['tiwe_cham_minOT_' num2str(100*patch_size_min) '_usetemp_' num2str(usetemp) '_patches_diffn2dtdzgamma_cnum_' num2str(cnum) '.mat']) )
+        
+        % add empty arrays for chi and eps
+        patches.eps=nan*ones(size(patches.p1));
+        patches.chi=nan*ones(size(patches.p1));
+        
+        % load the processed profile w/ chi and eps for each patch ('avg')
+        clear avg
+        fname=['tw91' sprintf('%04d',cnum) '_avg.mat'];
+        load(fullfile(data_dir,fname))
+        
+        patches.chi = avg.CHI(:);
+        patches.eps = avg.EPSILON(:);
+        
+        % compute gamma for each patch
+        patches.gam_range = ComputeGamma(patches.n2_range, patches.dtdz_range, patches.chi, patches.eps);
+        patches.gam_line  = ComputeGamma(patches.n2_line , patches.dtdz_line , patches.chi, patches.eps);
+        patches.gam_bulk  = ComputeGamma(patches.n2_bulk , patches.dtdz_bulk , patches.chi, patches.eps);
+        patches.gam_bulk_2= ComputeGamma(patches.n2_bulk_2, patches.dtdz_bulk, patches.chi, patches.eps);
+        patches.gam4 = ComputeGamma(patches.n4, patches.dtdz_line, patches.chi , patches.eps );
+        
+        % re-save profile
+        save(fullfile(save_dir_patch,['tiwe_cham_minOT_' num2str(100*patch_size_min) '_usetemp_' num2str(usetemp) '_patches_diffn2dtdzgamma_cnum_' num2str(cnum) '.mat']), 'patches' )
+        
+    end % try
+    
+end % cnum
+delete(hb)
+
+%%
