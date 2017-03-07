cbe6075e4c9a716515ea5f1886e1856a623a3502
andypicke <andypicke@gmail.com>
2017-03-07 11:36:28 -0700

    project conflicted w/ function of same name

--- Andy_Pickering/tiwe_patch_gamma/tiwe_patches_paths.m
+++ Andy_Pickering/tiwe_patch_gamma/tiwe_patches_paths.m
@@ -7,16 +7,16 @@
 %%
 
 analysis_dir = '/Users/Andy/Cruises_Research/Analysis/Andy_Pickering' ;
-project      = 'tiwe_patch_gamma' ;
+project_long      = 'tiwe_patch_gamma' ;
 project_short = 'tiwe'
 
-save_dir_patch = fullfile(analysis_dir,project,'data','patches') ;
+save_dir_patch = fullfile(analysis_dir,project_long,'data','patches') ;
 
-save_dir_avg   = fullfile(analysis_dir,project,'data','avg') ;
+save_dir_avg   = fullfile(analysis_dir,project_long,'data','avg') ;
 
-save_dir_avg_patch = fullfile(analysis_dir,project,'data','avg_patch') ;
+save_dir_avg_patch = fullfile(analysis_dir,project_long,'data','avg_patch') ;
 
-save_dir_cal = fullfile(analysis_dir,project,'data','cal') ;
+save_dir_cal = fullfile(analysis_dir,project_long,'data','cal') ;
 
 %%
 
