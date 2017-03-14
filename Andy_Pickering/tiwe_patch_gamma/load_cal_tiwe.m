425e8532a3803c8846b54f2c680ed2f4800d7aef
andypicke <andypicke@gmail.com>
2017-03-14 15:22:08 -0600

    function to load chameleon profile for tiwe

---
+++ Andy_Pickering/tiwe_patch_gamma/load_cal_tiwe.m
@@ -0,0 +1,26 @@
+function cal = load_cal_tiwe(cnum)
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+%
+% cal = load_cal_tiwe(cnum)
+%
+% Load high-res/raw Chameleon cast from tiwe ('cal'); this is calibrated but not
+% averaged/binned
+%
+% These are made in Process_tiwe_rawprofiles.m
+%
+% 3/14/17 - A.Pickering
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+%%
+
+tiwe_patches_paths
+load( fullfile( save_dir_cal, ['tw91' sprintf('%04d',cnum) '_raw.mat'] ) )
+cal=cal2 ; clear cal2
+
+% get latitude for profile
+clear idot lat1 lat2
+idot=strfind(head.lat.start,'.');
+lat1=str2num(head.lat.start(1:idot-3));
+lat2=str2num(head.lat.start(idot-2:end))/60;
+cal.lat=nanmean([lat1 lat2]);
+
+%%
