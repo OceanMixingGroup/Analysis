d333465d0c24be99d6b501c3db1459c80cdd655b
andypicke <andypicke@gmail.com>
2017-03-15 15:57:00 -0600

    remove un-used code

--- Andy_Pickering/gen_mfiles/compute_Tz_N2_for_patch.m
+++ Andy_Pickering/gen_mfiles/compute_Tz_N2_for_patch.m
@@ -17,15 +17,14 @@ function [out]=compute_Tz_N2_for_patch(p1,p2,p,t,s,ptmp,sgth,alpha,Lt)
 % n2_bulk
 % n4
 %
-%
+%-------------------
 % 2/28/17 A. Pickering
 %~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 %%
 
-
 % get indices of data in patch
 clear iz
-iz=isin(p,[ p1 p2 ]);
+iz = isin(p,[ p1 p2 ]) ;
 
 if length(iz)>10
     
@@ -43,15 +42,7 @@ if length(iz)>10
     % sort potential temp
     clear t_pot t_pot_sort
     [ptmp_sort , Iptmp]=sort(ptmp_ot,1,'descend');
-    
-    clear DT dz dTdz
-    %dT = nanmax(ptmp_ot)-nanmin(ptmp_ot) ;
-    %dz = nanmax(p_ot)-nanmin(p_ot) ;
-    %dTdz = -dT/dz ;
-    
-    % save results
-    %dtdz_range = dTdz;
-    
+           
     % fit a line to pot. temp to get slope
     clear P dtdz_line
     P = polyfit(p_ot,ptmp_sort,1);
@@ -70,14 +61,7 @@ if length(iz)>10
     
     % sorth pot. dens.
     [sgth_sort , I]=sort(sgth_ot,1,'ascend');
-    
-    % try the range/dz method
-%     drho = nanmax(sgth_ot)-nanmin(sgth_ot);
-%     dz = nanmax(p_ot)-nanmin(p_ot);
-%     n2_1 = 9.81/nanmean(sgth_ot)*drho/dz;
-%     
-%     n2_range = n2_1;
-    
+        
     % fit a line to sgth
     clear P1
     P1 = polyfit(p_ot,sgth_sort,1);
@@ -86,23 +70,20 @@ if length(iz)>10
     clear drhodz n2_2 drho dz n2_3
     drhodz = -P1(1);
     drhodz_line = drhodz;
+    n2_line = -9.81/nanmean(sgth)*drhodz;
     
-    n2_2=-9.81/nanmean(sgth)*drhodz;
-    n2_line=n2_2;
-    
-    n2_bulk= -9.81 / nanmean(sgth) * alpha * t_rms / Lt    ;
-    
+    % compute bulk N^2 (need to fit 
+    n2_bulk= -9.81 / nanmean(sgth) * alpha * t_rms / Lt    ;    
     
     % compute N^2 w/ sw_bfrq
     clear n2
     n2 = sw_bfrq(s_ot(I),t_ot(I),p_ot,0.03);
-    n4=nanmean(n2);
-    
-    %out.dtdz_range = dtdz_range;
+    n4=nanmean(n2);    
+
+    % Organize results in 'out' structure
     out.dtdz_line  = dtdz_line;
     out.dtdz_bulk  = dtdz_bulk;
     
-    %out.n2_range = n2_range;
     out.n2_line  = n2_line;
     out.n2_bulk  = n2_bulk;
     out.n4 = n4;
