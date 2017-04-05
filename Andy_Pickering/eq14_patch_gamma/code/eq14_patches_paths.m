06dd19dede07af785972abc1cc7a3084bfc35607
andypicke <andypicke@gmail.com>
2017-04-05 09:58:16 -0600

    add paths

--- Andy_Pickering/eq14_patch_gamma/code/eq14_patches_paths.m
+++ Andy_Pickering/eq14_patch_gamma/code/eq14_patches_paths.m
@@ -1,23 +1,33 @@
-%
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 %
 % eq14_patches_paths.m
 %
 %
-%
+% 4/4/17 - A.Pickering
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 %%
 
 clear analysis_dir project_long project_short
 
-analysis_dir = '/Users/Andy/Cruises_Research/Analysis/Andy_Pickering' ;
-project_long      = 'eq14_patch_gamma' ;
-project_short= 'eq14' ;
+analysis_dir   = '/Users/Andy/Cruises_Research/Analysis/Andy_Pickering' ;
+project_long   = 'eq14_patch_gamma' ;
+project_short  = 'eq14' ;
+
 save_dir_patch = fullfile(analysis_dir,project_long,'data','patches') ;
 
-save_dir_avg   = fullfile(analysis_dir,project_long,'data','avg') ;
-
+% chameleon profiles processed for patches only
 save_dir_avg_patch = fullfile(analysis_dir,project_long,'data','avg_patch') ;
 
-save_dir_cal='/Users/Andy/Cruises_Research/ChiPod/Cham_Eq14_Compare/Data/Cham_proc_AP/cal' ;
+%save_dir_avg   = fullfile(analysis_dir,project_long,'data','avg') ;
+
+% chameleon raw T,S, etc.
+save_dir_cal = '/Users/Andy/Cruises_Research/ChiPod/Cham_Eq14_Compare/Data/Cham_proc_AP/cal/' ;
+
+% chipod method applied to 1m binned  data
+path_chipod_bin     = '/Users/Andy/Cruises_Research/ChiPod/Cham_Eq14_Compare/Data/Cham_proc_AP/chipod_method_bin/';
+
+% chameleon 1m avg processed
+path_cham_avg       = '/Users/Andy/Cruises_Research/ChiPod/Cham_Eq14_Compare/Data/chameleon/processed_AP_7hz/mat/';
 
 fig_dir = fullfile( analysis_dir,project_long,'figures');
 
