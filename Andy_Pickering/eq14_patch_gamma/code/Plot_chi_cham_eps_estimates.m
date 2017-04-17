096b3e4b3e4e9a3055d1e4968a5d36b397befd7d
andypicke <andypicke@gmail.com>
2017-04-17 16:55:18 -0600

    new

---
+++ Andy_Pickering/eq14_patch_gamma/code/Plot_chi_cham_eps_estimates.m
@@ -0,0 +1,22 @@
+%~~~~~~~~~~~~~~~~~~~~~~~~~
+%
+% Plot_chi_cham_eps_estimates.m
+%
+% Want to get all chipod and chameleon estimates for a group of profiles.
+% - Plot histogram of all data for a given depth bin.
+% - plot the profile-averaged values. 
+%
+% Goal is to try to see how chipod epsilons converge to chameleon epsilons
+% when profile-averaged.
+%
+%
+%---------------
+% 4/14/17 - A.Pickering
+%~~~~~~~~~~~~~~~~~~~~~~~~~
+%%
+
+clear ; close all
+
+
+
+%%
