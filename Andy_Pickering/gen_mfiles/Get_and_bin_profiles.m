6eeff0e80314dc874b23a4ab51d2af923e4c9b10
andypicke <andypicke@gmail.com>
2017-04-17 09:06:36 -0600

    new function, compile binned data from chi and cham profiles

---
+++ Andy_Pickering/gen_mfiles/Get_and_bin_profiles.m
@@ -0,0 +1,101 @@
+function [chipod, cham] =Get_and_bin_profiles(path_chipod_bin,path_cham_avg,dz,Params,cnums_to_get,project_short,zmin,zmax)
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+%
+% Compile data from binned chipod method and chameleon for specified
+% profiles, averaged in bins of size dz. For eq08 and eq14.
+%
+% Similar to Get_binned_profiles, but returns matrix of profiles instead of
+% one vector.
+%
+% - log10(chamleon epsilon) < -8.5 are discarded
+%
+%
+% INPUT
+% path_chipod_bin
+% path_cham_avg
+% dz
+% Params
+%
+% OUTPUT
+%
+%
+%
+%-----------------
+% 4/14/17 - A.Pickering
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+%%
+
+% Make empty arrays
+
+empty_array = nan * ones(length([zmin:dz:zmax]),length(cnums_to_get) );
+
+eps_chi  = empty_array ;
+eps_cham = empty_array ;
+
+chi_chi  = empty_array;
+chi_cham = empty_array;
+
+N2_chi  = empty_array;
+N2_cham = empty_array;
+
+Tz_chi  = empty_array;
+Tz_cham = empty_array;
+
+P_cham_avg = empty_array;
+P_chi_avg  = empty_array;
+
+
+for ic = 1:length(cnums_to_get)
+    
+    clear cnum
+    cnum = cnums_to_get(ic);
+    
+    clear avg ch chb
+    
+    try
+        
+        % regular chi-pod method on binned data
+        clear avg
+        if project_short=='eq14'
+            load( fullfile( path_chipod_bin, ['zsm1m_fmax' num2str(Params.fmax) 'Hz_respcorr0_fc_99hz_gamma' num2str(Params.gamma*100) '_nfft_128'],[upper(project_short) '_' sprintf('%04d',cnum) '_avg.mat']))
+        else
+            load( fullfile( path_chipod_bin, ['zsm1m_fmax' num2str(Params.fmax) 'Hz_respcorr0_fc_99hz_gamma' num2str(Params.gamma*100) '_nfft_128'],[project_short '_' sprintf('%04d',cnum) '_avg.mat']))
+        end
+        chb = avg ; clear avg
+        
+        % chamelon data (1m bins)
+        load(fullfile( path_cham_avg, [project_short '_' sprintf('%04d',cnum) '_avg.mat']) )
+        
+        %% discard chameleon epsilons below noise floor
+        
+        clear ib
+        ib = find( log10(avg.EPSILON)<-8.5 );
+        %avg.EPSILON(ib) = nan ;
+        
+        [eps_cham(:,ic) z1 Nobs] = binprofile(avg.EPSILON, avg.P, 0, dz, 200,0);
+        [eps_chi(:,ic)  z2 Nobs] = binprofile(chb.eps1   , chb.P, 0, dz, 200,0);
+                
+        [chi_cham(:,ic) z1 Nobs] = binprofile(avg.CHI, avg.P, 0, dz, 200,0);
+        [chi_chi(:,ic)  z2 Nobs] = binprofile(chb.chi1   , chb.P, 0, dz, 200,0);
+                
+        [N2_cham(:,ic) z1 Nobs] = binprofile(avg.N2, avg.P, 0, dz, 200,0);
+        [N2_chi(:,ic)  z2 Nobs] = binprofile(chb.N2   , chb.P, 0, dz, 200,0);
+                
+        [Tz_cham(:,ic) z1 Nobs] = binprofile(avg.DTDZ, avg.P, 0, dz, 200,0);
+        [Tz_chi(:,ic)  z2 Nobs] = binprofile(chb.dTdz   , chb.P, 0, dz, 200,0);
+                
+    catch
+        disp(['error on profile ' num2str(cnum) ])
+    end % try
+    
+end % cnum
+
+chipod = struct('eps',eps_chi,'chi',chi_chi,'N2',N2_chi,'Tz',Tz_chi);
+cham   = struct('eps',eps_cham,'chi',chi_cham,'N2',N2_cham,'Tz',Tz_cham);
+
+chipod.P = [zmin:dz:zmax]';
+chipod.cnum = cnums_to_get;
+cham.P = [zmin:dz:zmax]';
+cham.cnum = cnums_to_get ;
+
+%%
