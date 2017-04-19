74010c3423b2d54f124d48e82399cb6adbc52c90
andypicke <andypicke@gmail.com>
2017-04-19 15:37:53 -0600

    make function for normalized chi vs eps plot

---
+++ Andy_Pickering/gen_mfiles/chi_vs_eps_normalized_plot.m
@@ -0,0 +1,16 @@
+function h = chi_vs_eps_normalized_plot(eps,chi,N2,Tz)
+
+h=histogram2(  real(log10(eps./N2)),log10(chi./(Tz.^2)),80,'DisplayStyle','tile');
+grid on
+hold on
+xvec=linspace(1e-7,1e-1,100);
+h1=plot( log10(xvec), log10(xvec*2*0.2),'k-');
+h2=plot( log10(xvec), log10(xvec*2*0.1),'r-');
+h3=plot( log10(xvec), log10(xvec*2*0.05),'c-');
+ylim([-7.5 -1])
+xlim([-5.5 -1])
+ylabel('log_{10} [\chi / T_{z}^{2}]','fontsize',16)
+xlabel('log_{10} [\epsilon / N^{2}]','fontsize',16)
+legend([h1 h2 h3],['\gamma=0.2'],['\gamma=0.1'],['\gamma=0.05'],'location','best')
+
+%%
