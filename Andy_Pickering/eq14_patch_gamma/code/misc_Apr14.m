e36611e308e2329385ed2be1f417f5e88357b102
andypicke <andypicke@gmail.com>
2017-04-26 13:47:12 -0600

    update overview notes

--- Andy_Pickering/eq14_patch_gamma/code/misc_Apr14.m
+++ Andy_Pickering/eq14_patch_gamma/code/misc_Apr14.m
@@ -73,6 +73,8 @@ histogram2( log10(cham.chi), log10(chipod.chi), 'DisplayStyle','tile')
 hold on
 xvec=linspace(-11,-4,100);
 plot(xvec,xvec,'k--')
+plot(xvec,xvec-1,'r--')
+plot(xvec,xvec+1,'r--')
 xlim([-12 -4])
 ylim([-12 -4])
 xlabel('\chi chameleon')
@@ -83,6 +85,9 @@ histogram2( log10(cham.eps), log10(chipod.eps),50, 'DisplayStyle','tile')
 hold on
 xvec=linspace(-11,-4,100);
 plot(xvec,xvec,'k--')
+plot(xvec,xvec-1,'r--')
+plot(xvec,xvec+1,'r--')
+
 xlim([-12 -4])
 ylim([-12 -4])
 xlabel('\epsilon chameleon')
