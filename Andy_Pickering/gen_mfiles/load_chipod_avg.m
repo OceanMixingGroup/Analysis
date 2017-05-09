ffda2b7aee45a3d35e4a0782f69c9fd9bbf21d2f
andypicke <andypicke@gmail.com>
2017-05-09 15:09:45 -0600

    new general function to load processed chipped profiles

---
+++ Andy_Pickering/gen_mfiles/load_chipod_avg.m
@@ -0,0 +1,18 @@
+function chb = load_chipod_avg(path_chipod_bin,project_short,Params,cnum)
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+%
+%
+%
+% 5/9/17 - A.Pickering
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+%%
+
+clear avg
+if strcmp(project_short,'eq14')
+    load( fullfile( path_chipod_bin, ['zsm' num2str(Params.z_smooth) 'm_fmax' num2str(Params.fmax) 'Hz_respcorr0_fc_99hz_gamma' num2str(Params.gamma*100) '_nfft_128'],[upper(project_short) '_' sprintf('%04d',cnum) '_avg.mat']))
+else
+    load( fullfile( path_chipod_bin, ['zsm' num2str(Params.z_smooth) 'm_fmax' num2str(Params.fmax) 'Hz_respcorr0_fc_99hz_gamma' num2str(Params.gamma*100) '_nfft_128'],[project_short '_' sprintf('%04d',cnum) '_avg.mat']))
+end
+chb = avg;clear avg
+
+%%
