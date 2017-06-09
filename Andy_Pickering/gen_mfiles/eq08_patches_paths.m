5b199419b9ad47863400486f6a33378f0af96024
andypicke <andypicke@gmail.com>
2017-06-09 14:43:10 -0600

    update eq08 paths

--- Andy_Pickering/gen_mfiles/eq08_patches_paths.m
+++ Andy_Pickering/gen_mfiles/eq08_patches_paths.m
@@ -17,20 +17,20 @@ analysis_dir = '/Users/Andy/Cruises_Research/Analysis/Andy_Pickering' ;
 save_dir_patch = fullfile(analysis_dir,project_long,'data','patches') ;
 ChkMkDir(save_dir_patch)
 
-save_dir_avg   = fullfile(analysis_dir,project_long,'data','avg') ;
+save_dir_avg   = fullfile(analysis_dir,project_short,'data','avg') ;
 %ChkMkDir(save_dir_avg)
 
-save_dir_avg_patch = fullfile(analysis_dir,project_long,'data','avg_patch') ;
+save_dir_avg_patch = fullfile(analysis_dir,project_short,'data','avg_patch') ;
 ChkMkDir(save_dir_avg_patch)
 
 save_dir_cal = '/Users/Andy/Cruises_Research/Analysis/Andy_Pickering/EQ08/Data/cham_proc/cal/' ;
 
-fig_dir = fullfile(analysis_dir,project_long,'figures');
+fig_dir = fullfile(analysis_dir,project_short,'figures');
 ChkMkDir(fig_dir)
 
 % chameleon 1m avg processed
 %path_cham_avg       = '/Users/Andy/Cruises_Research/ChiPod/EQ08/Data/cham_proc/avg/';
-path_cham_avg       = '/Users/Andy/Cruises_Research/ChiPod/EQ08/Data/cham_proc_AP_10hz/avg/';
+path_cham_avg       = '/Users/Andy/Cruises_Research/Analysis/Andy_Pickering/EQ08/Data/cham_proc_AP_10hz/avg/';
 
 % chipod method applied to 1m binned  data
 path_chipod_bin     = '/Users/Andy/Cruises_Research/Analysis/Andy_Pickering/EQ08/Data/cham_proc/chipod_method_bin/';
