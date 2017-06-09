209ef225f8135286fdaa492c681dc57f1cfd4c9c
andypicke <andypicke@gmail.com>
2017-06-09 13:31:33 -0600

    modify paths and move to gen folder

--- Andy_Pickering/gen_mfiles/eq08_patches_paths.m
+++ Andy_Pickering/gen_mfiles/eq08_patches_paths.m
@@ -2,6 +2,7 @@
 %
 % eq08_patches_paths.m
 %
+% Set paths for processing/analysis related to EQ08 chameleon data
 %
 %--------------
 % 2/27/17 - A.Pickering
@@ -22,7 +23,7 @@ save_dir_avg   = fullfile(analysis_dir,project_long,'data','avg') ;
 save_dir_avg_patch = fullfile(analysis_dir,project_long,'data','avg_patch') ;
 ChkMkDir(save_dir_avg_patch)
 
-save_dir_cal = '/Users/Andy/Cruises_Research/ChiPod/EQ08/Data/cham_proc/cal/' ;
+save_dir_cal = '/Users/Andy/Cruises_Research/Analysis/Andy_Pickering/EQ08/Data/cham_proc/cal/' ;
 
 fig_dir = fullfile(analysis_dir,project_long,'figures');
 ChkMkDir(fig_dir)
@@ -32,7 +33,7 @@ ChkMkDir(fig_dir)
 path_cham_avg       = '/Users/Andy/Cruises_Research/ChiPod/EQ08/Data/cham_proc_AP_10hz/avg/';
 
 % chipod method applied to 1m binned  data
-path_chipod_bin     = '/Users/Andy/Cruises_Research/ChiPod/EQ08/Data/cham_proc/chipod_method_bin/';
+path_chipod_bin     = '/Users/Andy/Cruises_Research/Analysis/Andy_Pickering/EQ08/Data/cham_proc/chipod_method_bin/';
 
 
 %%
