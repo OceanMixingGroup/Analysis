16fb16955619025e171c8e21f87f257741731c66
andypicke <andypicke@gmail.com>
2017-06-12 11:14:12 -0600

    update paths

--- Andy_Pickering/EQ08/mfiles/discard_convection_eq08_chi.m
+++ Andy_Pickering/EQ08/mfiles/discard_convection_eq08_chi.m
@@ -7,7 +7,7 @@ function avg = discard_convection_eq08_chi(avg,cnum)
 %~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 %%
 
-load('/Users/Andy/Cruises_Research/Analysis/Andy_Pickering/eq08_patch_gamma/data/eq08_mldepths.mat')
+load('/Users/Andy/Cruises_Research/Analysis/Andy_Pickering/eq08/data/eq08_mldepths.mat')
 
 try
     id = find( cnum==zml_cnum ) ;
--- Andy_Pickering/gen_mfiles/discard_convection_eq14_chi.m
+++ Andy_Pickering/gen_mfiles/discard_convection_eq14_chi.m
@@ -7,7 +7,7 @@ function avg = discard_convection_eq14_chi(avg,cnum)
 %~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 %%
 
-load('/Users/Andy/Cruises_Research/Analysis/Andy_Pickering/eq14_patch_gamma/data/EQ14_mldepths.mat')
+load('/Users/Andy/Cruises_Research/Analysis/Andy_Pickering/eq14/data/EQ14_mldepths.mat')
 
 try
     id = find( cnum==zml_cnum ) ;
