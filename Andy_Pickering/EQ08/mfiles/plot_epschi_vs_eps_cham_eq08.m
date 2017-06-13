7eb688c675f431f7522432f3e70218701037d8fa
andypicke <andypicke@gmail.com>
2017-06-13 11:13:35 -0600

    update figure and notes

--- Andy_Pickering/EQ08/mfiles/plot_epschi_vs_eps_cham_eq08.m
+++ Andy_Pickering/EQ08/mfiles/plot_epschi_vs_eps_cham_eq08.m
@@ -10,11 +10,11 @@
 %~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 %%
 
-clear ; close all
+clear ; %close all
 
 Params.gamma    = 0.2;
 Params.fmax     = 10  ;
-Params.z_smooth = 1 ;
+Params.z_smooth = 10 ;
 Params.resp_corr= 0  ;
 Params.fc       = 99 ;
 
@@ -22,8 +22,8 @@ dz = 2 ;
 cnums_to_get = 200:2700;
 
 screen_chi= 1 ;
-screen_ml = 0 ;
-Pmin      = 0 ;
+screen_ml = 1 ;
+Pmin      = 20 ;
 
 addpath /Users/Andy/Cruises_Research/Analysis/Andy_Pickering/gen_mfiles/
 addpath /Users/Andy/Cruises_Research/mixingsoftware/CTD_Chipod/mfiles/
@@ -38,11 +38,12 @@ end
 
 eps_chi = cham.chi .* cham.N2 / 2 / 0.2 ./ (cham.Tz.^2);
 
-figure(1);clf
+figure%(1);clf
 agutwocolumn(0.6)
 wysiwyg
 
-histogram2( real(log10(cham.eps)), real(log10(eps_chi)), 'DisplayStyle','tile','EdgeColor','none')
+subplot(1,3,[1 2])
+histogram2( real(log10(cham.eps)), real(log10(eps_chi)), 'DisplayStyle','tile','EdgeColor','none','XBinEdges',[-8.5:0.2:-5],'YBinEdges',[-11:0.2:-5])
 xlim([-8.5 -5])
 ylim([-11 -5])
 xvec = linspace(-8.5,-5,100);
@@ -50,11 +51,21 @@ hold on
 plot(xvec,xvec,'k--')
 plot(xvec,xvec+1,'r--')
 plot(xvec,xvec-1,'r--')
-xlabel('\epsilon','fontsize',16)
-ylabel('\epsilon_{\chi}','fontsize',16)
+xlabel('log_{10}[\epsilon]','fontsize',16)
+ylabel('log_{10}[\epsilon_{\chi}]','fontsize',16)
+
+
+subplot(1,3,3)
+histogram( real(log10( eps_chi(:) ./ cham.eps(:))),'edgecolor','none','Normalization','pdf')
+grid on
+freqline( nanmean(real(log10( eps_chi(:) ./ cham.eps(:)))))
+xlim([-3.5 2])
+xlabel('log_{10}[\epsilon_{\chi}/\epsilon]','fontsize',16)
 
 eq08_patches_paths
 figname = [project_short '_epschi_vs_eps']
 print(fullfile(fig_dir, figname), '-dpng')
+
+
 
 %%
